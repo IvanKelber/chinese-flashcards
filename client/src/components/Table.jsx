461802cb3ea2a4b6dec189361c80993ea7ff9272
Ivan Kelber <ivankelber@gmail.com>
2018-02-05 15:21:09 -0500

    stylized table a bit.  added a new word button with no functionality

--- client/src/components/Table.jsx
+++ client/src/components/Table.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
-import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
+import {Button} from 'react-bootstrap'
+import {BootstrapTable, TableHeaderColumn, InsertButton} from 'react-bootstrap-table';
 import '../../node_modules/react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
+import './Table.css'
+
 
 class Table extends Component {
   state = {
@@ -25,6 +28,16 @@ class Table extends Component {
 
       return body;
   };
+  handleNewWordClick = (onClick) =>{
+      console.log('clicked');
+      console.log(onClick);
+  }
+
+  createNewWordButton = (onClick) => {
+    return (
+      <Button bsStyle="success" onClick={this.handleNewWordClick}>New Word</Button>
+    );
+  }
 
   render(){
     var wordsJSON = this.state.words.map(function(word) {
@@ -34,14 +47,22 @@ class Table extends Component {
             definition: word[2],
             particle: word[3]
         }
-    })
+    });
+
+    const options = {
+        insertBtn: this.createNewWordButton
+    }
+
     return (
-        <BootstrapTable data={wordsJSON} search >
+        <div className="TableContainer">
+        <BootstrapTable data={wordsJSON} options={options}
+            search striped insertRow maxHeight='400px'>
             <TableHeaderColumn isKey dataField="character"> 汉字 </TableHeaderColumn>
             <TableHeaderColumn dataField="pinyin"> Pinyin </TableHeaderColumn>
             <TableHeaderColumn dataField="definition"> Definition </TableHeaderColumn>
             <TableHeaderColumn dataField="particle"> Particle </TableHeaderColumn>
         </BootstrapTable>
+        </div>
 )
   }
 }
