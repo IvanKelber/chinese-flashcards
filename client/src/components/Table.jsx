aa3ce588c5d6b2bd5dfeabd11b7284f33defa88e
Ivan Kelber <ivankelber@gmail.com>
2018-02-05 18:36:03 -0500

    added modal to new word button

--- client/src/components/Table.jsx
+++ client/src/components/Table.jsx
@@ -3,11 +3,45 @@ import {Button} from 'react-bootstrap'
 import {BootstrapTable, TableHeaderColumn, InsertButton} from 'react-bootstrap-table';
 import '../../node_modules/react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
 import './Table.css'
+import Modal from "react-overlays/lib/Modal";
 
+
+const modalStyle = {
+  position: 'fixed',
+  zIndex: 1040,
+  top: 0, bottom: 0, left: 0, right: 0,
+};
+
+const backdropStyle = {
+  ...modalStyle,
+  zIndex: 'auto',
+  backgroundColor: 'lightblue',
+  opacity: 0.5
+};
+
+const dialogStyle = function() {
+  // we use some psuedo random coords so nested modals
+  // don't sit right on top of each other.
+  let top = 50
+  let left = 50
+
+  return {
+    position: 'absolute',
+    width: '500px',
+    height: '400px',
+    top: top + '%', left: left + '%',
+    transform: `translate(-${top}%, -${left}%)`,
+    border: '1px solid #e5e5e5',
+    backgroundColor: 'white',
+    boxShadow: '0 5px 15px rgba(0,0,0,.5)',
+    padding: 20
+  };
+};
 
 class Table extends Component {
   state = {
-      words: []
+      words: [],
+      modal_open: false
   };
 
   componentDidMount() {
@@ -31,12 +65,21 @@ class Table extends Component {
   handleNewWordClick = (onClick) =>{
       console.log('clicked');
       console.log(onClick);
+      this.setState({
+          modal_open: true
+      })
   }
 
   createNewWordButton = (onClick) => {
     return (
       <Button bsStyle="success" onClick={this.handleNewWordClick}>New Word</Button>
     );
+  }
+
+  closeModal = () => {
+      this.setState({
+          modal_open: false
+      })
   }
 
   render(){
@@ -55,13 +98,23 @@ class Table extends Component {
 
     return (
         <div className="TableContainer">
-        <BootstrapTable data={wordsJSON} options={options}
-            search striped insertRow maxHeight='400px'>
-            <TableHeaderColumn isKey dataField="character"> 汉字 </TableHeaderColumn>
-            <TableHeaderColumn dataField="pinyin"> Pinyin </TableHeaderColumn>
-            <TableHeaderColumn dataField="definition"> Definition </TableHeaderColumn>
-            <TableHeaderColumn dataField="particle"> Particle </TableHeaderColumn>
-        </BootstrapTable>
+            <Modal
+              style={modalStyle}
+              backdropStyle={backdropStyle}
+              autoFocus={false}
+              show={this.state.modal_open}
+              onHide={this.closeModal}>
+              <div style={dialogStyle()}>
+                  Hello Darkness My Old Friend
+              </div>
+            </Modal>
+            <BootstrapTable data={wordsJSON} options={options}
+                search striped insertRow maxHeight='400px'>
+                <TableHeaderColumn isKey dataField="character"> 汉字 </TableHeaderColumn>
+                <TableHeaderColumn dataField="pinyin"> Pinyin </TableHeaderColumn>
+                <TableHeaderColumn dataField="definition"> Definition </TableHeaderColumn>
+                <TableHeaderColumn dataField="particle"> Particle </TableHeaderColumn>
+            </BootstrapTable>
         </div>
 )
   }
