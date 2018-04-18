1038d7a176966a13b73e88b3969ace70921806bd
Ivan Kelber <ivankelber@gmail.com>
2018-04-17 22:46:51 -0400

    added difficulties to cards.  clicking cycles through difficulty.  the CSS is hacky af

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -2,41 +2,49 @@ import React, { Component } from 'react';
 
 class Card extends Component {
 
-    initialState() {
-        return({
-            character: "",
-            pinyin: "",
-            definition: "",
-            particle: "",
-            difficulty: 0
-        });
-    };
+    constructor() {
+        super();
+        this.handleClick = this.handleClick.bind(this);
+    }
 
+    state = {
+            difficulty: 0,
+            word: [],
+            visibleWord: []
+    };
 
     componentDidMount() {
         if(this.props.word !== undefined) {
             this.setState({
-                character: this.props.word[0],
-                pinyin: this.props.word[1],
-                definition: this.props.word[2],
-                particle: this.props.word[3],
+                word: this.props.word
             });
         }
 
     };
 
-
+    handleClick() {
+        var newDifficulty = (this.state.difficulty + 1) % 4;
+        this.setState({
+            difficulty: newDifficulty,
+            visibleWord: this.state.word.slice(1, newDifficulty + 1)
+        });
+    };
 
     render() {
         if(this.props.word === undefined) {
             return null;
         }
+        var pinyinClass = this.state.difficulty >= 1 ? "visible" : "hidden";
+        var particleClass = this.state.difficulty >= 2 ? "visible" : "hidden";
+        var definitionClass = this.state.difficulty >= 3 ? "visible" : "hidden";
+
         return(
-            <span className="Card">
-                <h1>{this.props.word[0]}</h1>
-                <p>{this.props.word[1]}</p>
-                <p>{this.props.word[2]}</p>
-                <p>{this.props.word[3]}</p>
+            <span className="Card" onClick={this.handleClick}>
+                <h1>{this.state.word[0]}</h1>
+                <p className={pinyinClass}>{this.state.word[1]}</p>
+                <p className={particleClass}>{this.state.word[2]}</p>
+                <p className={definitionClass}>{this.state.word[3]}</p>
+
             </span>
 
         );
